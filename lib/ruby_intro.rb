3acf5012d1491c7f1d80fa463cd474651a9ad3af
Mason Becker <mason@localhost.localdomain>
2018-02-02 13:25:28 -0600

    Implement all missing functionality

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,46 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.sort.reverse.take(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  arr.combination(2).map { |u| u.reduce(:+) }.include?(n)
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match?(/^[A-Za-z]/) and s.downcase.match?(/^[^aeiou]/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if !s.match(/^[01]+$/)
+  s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    @isbn = isbn
+    raise ArgumentError if price <= 0
+    @price = price
+  end
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
